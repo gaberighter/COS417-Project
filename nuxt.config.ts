7f85a9aa55b8f7215277a93f4e66763ea6c0f495
jacobeldridge <47110405+jacobeldridge@users.noreply.github.com>
2026-04-08 13:37:12 -0400

    kepler route hotfix (#13)

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
+  app: {
+    baseURL: '/COS170/'
+  },
   devtools: { enabled: true }
 })
