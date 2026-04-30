1dfd14e70c123be74173f2fa39043e9134ed9731
GrantWidener <156404983+GrantWidener@users.noreply.github.com>
2026-04-30 15:18:04 +0000

    Remove debug buttons.
     Changes to be committed:
    	modified:   app/pages/auth/login.vue

--- app/pages/auth/login.vue
+++ app/pages/auth/login.vue
@@ -6,10 +6,6 @@
     </button>
     <p v-if="error" class="error">{{ error }}</p>
   </div>
-  <div class="temporary-debug-buttons">
-    <button @click="redirectToAdmin">Admin Page</button>
-    <button @click="redirectToFaculty">Faculty Page</button>
-  </div>
 </template>
 
 <script setup lang="ts">
@@ -19,7 +15,6 @@ definePageMeta({
 })
 
 const { loginWithSso, isLoading, error } = useAuth()
-const { redirectToAdmin, redirectToFaculty } = useDebugNavigation()
 </script>
 
 <style>
